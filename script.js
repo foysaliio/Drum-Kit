1b03ba2bea9430bbe3badcfe2438bfca4ac26b9c
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-14 15:25:16 +0600

    feat: implement drum sound playback

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function playSound(key) {
+  let audio;
+
+  switch (key) {
+    case 'w':
+      audio = new Audio('./sounds/tom-1.mp3');
+      break;
+
+    case 'a':
+      audio = new Audio('./sounds/tom-2.mp3');
+      break;
+
+    case 's':
+      audio = new Audio('./sounds/tom-3.mp3');
+      break;
+
+    case 'd':
+      audio = new Audio('./sounds/tom-4.mp3');
+      break;
+
+    case 'j':
+      audio = new Audio('./sounds/snare.mp3');
+      break;
+
+    case 'k':
+      audio = new Audio('./sounds/crash.mp3');
+      break;
+
+    case 'l':
+      audio = new Audio('./sounds/kick-bass.mp3');
+      break;
+
+    default:
+      return;
+  }
+
+  audio.play();
+}
